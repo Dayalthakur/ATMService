4053d5abd2561f3bc825bffc5a83ded5739ce8a0
Dayal Thakur <thakurdaya2001@gmail.com>
2025-09-21 08:04:16 +0530

    first commit

---
+++ Implementation/Program.cs
@@ -0,0 +1,80 @@
+﻿namespace Implementation
+{
+    public class Program
+    {
+        public class Encapsulate
+        {
+            private int _balance;
+            public Encapsulate()
+            {
+                _balance = 0;
+            }
+            public int GetBalance()
+            {
+                if (_balance < 0)
+                {
+                    return 0;
+                }
+                return _balance;
+            }
+            public void Deposit(int amount)
+            {
+                _balance += amount;
+                Console.WriteLine($"Amount Deposited: {amount}");
+                Console.WriteLine($"New Balance {_balance}");
+            }
+            public void WithDraw(int amount)
+            {
+                if (amount <= _balance)
+                {
+                    _balance -= amount;
+                    Console.WriteLine($"Amount Withdrawn :{amount}");
+                    Console.WriteLine($"New Balance : {_balance}");
+                }
+            }
+        }
+
+        public static void Main(string[] strings)
+        {
+            Encapsulate en = new Encapsulate();
+            while (true)
+            {
+                Console.WriteLine("\n------ MENU ------");
+                Console.WriteLine("1. Get Balance");
+                Console.WriteLine("2. Withdraw Cash");
+                Console.WriteLine("3. Deposit Cash");
+                Console.WriteLine("4. Exit");
+                Console.Write("Kindly select any option to proceed: ");
+                int option;
+                if (!int.TryParse(Console.ReadLine(), out option))
+                {
+                    Console.WriteLine("Invalid input. Please enter a number.");
+                }
+                switch (option)
+                {
+                    case 1:
+                        Console.WriteLine($"Your Balance is :{en.GetBalance()} ");
+                        break;
+                    case 2:
+                        Console.WriteLine("Kindly provide the amount to withdraw : ");
+                        int withdraw = Convert.ToInt32(Console.ReadLine());
+                        en.WithDraw(withdraw);
+                        break;
+                    case 3:
+                        Console.WriteLine("Provide the amount you want to deposit : ");
+                        int deposit = Convert.ToInt32(Console.ReadLine());
+                        en.Deposit(deposit);
+                        break;
+                    case 4:
+                        Console.WriteLine("Thank you for using our services");
+                        return;
+                    default:
+                        Console.WriteLine("Please select proper option");
+                        break;
+                }
+
+            }
+
+        }
+    }
+}
